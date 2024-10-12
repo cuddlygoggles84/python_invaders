4da2e0bc13fe5ab3783dcaab7f41d61a7550a37d
… <gabeamador@CACA.local>
2024-10-12 11:22:27 -0700

    first commit

---
+++ invaders.py
@@ -0,0 +1,46 @@
+import pygame, sys
+from spaceship import Spaceship
+
+pygame.init()
+
+screen_x = 750
+
+screen_y = 700
+
+GREY = (25, 25, 25)
+
+SCREEN = pygame.display.set_mode((screen_x, screen_y))
+
+spaceship = Spaceship(screen_x, screen_y)
+
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
+
+
+
+
+pygame.display.set_caption("Python Invaders")   
+
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            sys.exit()
+
+    #update movement
+    spaceship_group.update()
+
+    #draw
+    SCREEN.fill(GREY)
+    spaceship_group.draw(SCREEN)
+    pygame.display.update()  
+          
+    clock.tick(60)
+
+
+
+
+
---
+++ spaceship.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Spaceship (pygame.sprite.Sprite):
+    def __init__(self, screen_x, screen_y):
+        self.screen_x = screen_x
+        self.screen_y = screen_y
+        super().__init__()
+        self.image = pygame.image.load("sprites/spaceship.png")
+        self.rect = self.image.get_rect(midbottom = (self.screen_x/2,self.screen_y))
+        self.speed = 6
+    
+
+    def get_user_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys [pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        if keys [pygame.K_LEFT]:
+            self.rect.x -= self.speed
+
+    
+    def update(self):
+        self.get_user_input()
+
+
+
+
+
+
+
